73997381c469a524ff70c18e9c91abdd030a2284
Lauryn <laurynpearson@gmail.com>
2019-08-01 20:54:07 -0500

    create create action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="home">
     <h1>All Contacts</h1>
+    <p>First Name: <input type="text" v-model="newContactFirstName"></p>
+    <p>Last Name: <input type="text" v-model="newContactLastName"></p>
+    <p>Email: <input type="text" v-model="newContactEmail"></p>
+    <p>Phone Number: <input type="text" v-model="newContactPhoneNumber"></p>
+    <button v-on:click="createContact()">Create Contact</button>
+    <hr>
     <div v-for="contact in contacts">
       <p>{{ contact.first_name }}</p>
       <p>{{ contact.last_name }}</p>
       <p>{{ contact.email }}</p>
       <p>{{ contact.phone_number }}</p>
+      <hr>
     </div>
   </div>
 </template>
@@ -17,7 +24,11 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      contacts: []
+      contacts: [],
+      newContactFirstName: "",
+      newContactLastName: "",
+      newContactEmail: "",
+      newContactPhoneNumber: ""
     };
   },
   created: function() {
@@ -25,6 +36,22 @@ export default {
       this.contacts = response.data;
     });
   },
-  methods: {}
+  methods: {
+    createContact: function() {
+      var params = {
+        first_name: this.newContactFirstName,
+        last_name: this.newContactLastName,
+        email: this.newContactEmail,
+        phone_number: this.newContactPhoneNumber
+      };
+      axios.post('/api/contacts', params).then(response => {
+        this.contacts.push(response.data);
+        this.newContactFirstName = "";
+        this.newContactLastName = "";
+        this.newContactEmail = "";
+        this.newContactPhoneNumber = "";
+      });
+    }
+  }
 };
 </script>
